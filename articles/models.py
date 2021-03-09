c57fc3dbf06eb9577a03113d23154dbe316660dc
pvparty <fake@example.com>
2021-03-09 13:39:37 -0600

    migrate Article model

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = mdoels.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
