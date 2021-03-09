0988828ba8652dd5bafefff877d7bb35d6347ffb
pvparty <fake@example.com>
2021-03-09 13:25:01 -0600

    create view function

--- net_ninja/settings.py
+++ net_ninja/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'articles',
+
+    # Django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
