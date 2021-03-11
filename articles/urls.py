fca7f05578941c0c1c309ef97a522beebca2b9a0
pvparty <fake@example.com>
2021-03-11 09:46:15 -0600

    reorganize

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list, name='article_list')
+    path('', views.article_list, name='list'),
 ]
--- net_ninja/settings.py
+++ net_ninja/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # My apps.
-    'articles',
+    'articles.apps.ArticlesConfig',
 
     # Django apps.
     'django.contrib.admin',
