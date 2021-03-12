000fc897a24c566316e3e1bce779ec6b19f75960
pvparty <fake@example.com>
2021-03-12 13:44:25 -0600

    log in form and template

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 
 urlpatterns =[
     path('signup/', views.sign_up, name='sign_up'),
+    path('login/', views.login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def sign_up(request):
@@ -13,3 +13,12 @@ def sign_up(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
