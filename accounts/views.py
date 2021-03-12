17b93422c30d39134a0106720a244c4ca2d5b2c5
pvparty <fake@example.com>
2021-03-12 11:58:19 -0600

    create user creation form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def sign_up(request):
-    return render(request, 'accounts/signup.html')
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
